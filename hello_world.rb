7ffaea734d316233cd1b7664cfeacb2ea1680747
edouard winia <edouardwinia@gmail.com>
2016-03-20 21:51:44 +0100

    what to do

---
+++ hello_world.rb
@@ -0,0 +1,56 @@
+# - une navbar avec les liens vers le différentes pages
+# - 1 champs de recherche - gem elastic search
+# - des users non loggués qui cherchent des annonces
+# - des users non logués qui déposent des annonces avec upload de photos
+# - des users loggués qui ont accès à un tableau de bord
+# - 1 pannel avec des liens vers les catégories de résultats de recherche
+
+
+# Composants FRONT :
+# - navbar
+# - footer
+# - panel champs de recherche
+# - panel résultat de recherche
+# - carte résultat
+# - carte annonce à la une
+# - panel annonces à la une
+# - panel liens a vers catégories de résultats
+# - panel déposer une annonce
+
+
+# vue par composants front
+# home :
+# navbar
+# liens résultats offres par catégories
+
+# déposer une annonce :
+# panel déposer une annonce
+
+# offres :
+# champs de recherche
+# panel résultats
+# carte résultats
+# pannel anonce à la une
+# carte annonce à la une
+
+# demandes :
+# champs de recherche
+# panel résultats
+# carte résultats
+# pannel anonce à la une
+# carte annonce à la une
+
+# mes annonces :
+# disponibles sous 'se connecter' seulement
+# pannel tableau de bord avec index des anonces de l'utilisateur
+
+
+# vue par composants :
+# - résultats de recherche -> tabs BOOTSTRAP
+# - champs de recherche -> postgre_search search Q?
+# - interface loguée -> devise
+# - déposer une annonce
+#   -> simple form
+#   -> upload de photos -> Q?
+# - pagination -> composant BOOTSTRAP pagination pour le front, gem will_paginate pour le back ?
+# - page aide : jquery pour les div macro, BOOTSTRAP collapse pour chaque question
